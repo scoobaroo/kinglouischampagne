1e6688c5e2e8d73bda9a9d1acdbce969f4553cc9
scoobaroo <scoobaroo415@gmail.com>
2016-06-03 23:48:06 -0700

    added app.listen to server.js

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ var app = express();
 app.get('/', function homepage (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
